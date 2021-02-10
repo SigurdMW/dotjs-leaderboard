db0a272cdc0dc0ae69ab67072f780965d0b20baf
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2021-02-10 17:08:07 +0100

    removed additional h1 in forms

--- app/auth/components/SignupForm.tsx
+++ app/auth/components/SignupForm.tsx
@@ -13,31 +13,27 @@ export const SignupForm = (props: SignupFormProps) => {
   const [signupMutation] = useMutation(signup)
 
   return (
-    <div>
-      <h1>Create an Account</h1>
-
-      <Form
-        submitText="Create Account"
-        schema={SignupInput}
-        initialValues={{ email: "", password: "" }}
-        onSubmit={async (values) => {
-          try {
-            await signupMutation(values)
-            props.onSuccess?.()
-          } catch (error) {
-            if (error.code === "P2002" && error.meta?.target?.includes("email")) {
-              // This error comes from Prisma
-              return { email: "This email is already being used" }
-            } else {
-              return { [FORM_ERROR]: error.toString() }
-            }
+    <Form
+      submitText="Create Account"
+      schema={SignupInput}
+      initialValues={{ email: "", password: "" }}
+      onSubmit={async (values) => {
+        try {
+          await signupMutation(values)
+          props.onSuccess?.()
+        } catch (error) {
+          if (error.code === "P2002" && error.meta?.target?.includes("email")) {
+            // This error comes from Prisma
+            return { email: "This email is already being used" }
+          } else {
+            return { [FORM_ERROR]: error.toString() }
           }
-        }}
-      >
-        <LabeledTextField name="email" label="Email" placeholder="Email" />
-        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-      </Form>
-    </div>
+        }
+      }}
+    >
+      <LabeledTextField name="email" label="Email" placeholder="Email" />
+      <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
+    </Form>
   )
 }
 
