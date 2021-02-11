66ee013e9b74b45659fb0d2939095a0542e3bead
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2021-02-11 11:07:12 +0100

    section nine completed

--- app/actions/components/ActionForm.tsx
+++ app/actions/components/ActionForm.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react"
 import { LabeledTextField } from "app/components/LabeledTextField"
 import { Form } from "app/components/Form"
 import { ActionInputType, ActionInput } from "../validations"
+import UserSelect from "app/components/UserSelect"
+import ActivitySelect from "app/components/ActivitySelect"
 
 type ActionFormProps = {
   initialValues: Partial<ActionInputType>
@@ -12,8 +14,8 @@ type ActionFormProps = {
 export const ActionForm: FC<ActionFormProps> = (props) => {
   return (
     <Form submitText={props.submitText || "Create"} schema={ActionInput} {...props}>
-      <LabeledTextField name="userId" label="User" placeholder="User" type="number" />
-      <LabeledTextField name="activityId" label="Activity" placeholder="Activity" type="number" />
+      <UserSelect name="userId" label="User" placeholder="User" />
+      <ActivitySelect name="activityId" label="Activity" placeholder="Activity" />
       <LabeledTextField name="comment" label="Comment" placeholder="Comment" type="text" />
     </Form>
   )
---
+++ app/components/UserSelect.tsx
@@ -0,0 +1,82 @@
+import { User } from "db"
+import getUsers from "app/users/queries/getUsers"
+import { useQuery } from "blitz"
+import React, { FC, PropsWithoutRef, Suspense } from "react"
+import { Controller, useFormContext } from "react-hook-form"
+
+export interface UserSelectFieldProps extends PropsWithoutRef<JSX.IntrinsicElements["input"]> {
+  /** Field name. */
+  name: string
+  /** Field label. */
+  label: string
+  outerProps?: PropsWithoutRef<JSX.IntrinsicElements["div"]>
+  users: Array<Pick<User, "email" | "name" | "id">>
+}
+
+export const UserSelectField = React.forwardRef<HTMLInputElement, UserSelectFieldProps>(
+  ({ label, outerProps, name, placeholder, users, ...props }, ref) => {
+    const {
+      formState: { isSubmitting },
+      control,
+      errors,
+    } = useFormContext()
+    const error = Array.isArray(errors[name])
+      ? errors[name].join(", ")
+      : errors[name]?.message || errors[name]
+
+    return (
+      <div {...outerProps} className="mb-6 max-w-lg">
+        <Controller
+          control={control}
+          name={name}
+          render={({ onChange, ...rest }, { invalid, isTouched, isDirty }) => (
+            <label className="block w-full mb-1">
+              {label}
+              <select
+                className="text-black block w-full p-1 pl-2 rounded-sm mt-2"
+                disabled={isSubmitting}
+                aria-invalid={invalid}
+                placeholder={placeholder}
+                onChange={(e) => {
+                  const val = e.target.value
+                  if (!isNaN(parseInt(val, 10))) {
+                    onChange(parseInt(val, 10))
+                  } else {
+                    onChange(undefined)
+                  }
+                }}
+                {...rest}
+              >
+                <option value="">Select</option>
+                {users.map((u) => (
+                  <option key={u.id} value={u.id}>
+                    {u.name || u.email}
+                  </option>
+                ))}
+              </select>
+            </label>
+          )}
+        />
+
+        {error && (
+          <div role="alert" className="text-red-600">
+            {error}
+          </div>
+        )}
+      </div>
+    )
+  }
+)
+
+const UserSelectLoader: FC<Omit<UserSelectFieldProps, "users">> = (props) => {
+  const [users] = useQuery(getUsers, undefined)
+  return <UserSelectField users={users} {...props} />
+}
+
+const UserSelect: FC<Omit<UserSelectFieldProps, "users">> = (props) => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <UserSelectLoader {...props} />
+  </Suspense>
+)
+
+export default UserSelect
